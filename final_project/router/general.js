dcb289bfb5bee050d0b5dc6dd3a1370e279851fd
InhumanNick <schmutzigtier@gmail.com>
2025-01-22 15:05:57 -0500

    final push

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -28,7 +29,31 @@ public_users.get('/',function (req, res) {
    res.send(JSON.stringify(books,null,10))
 
   return res.status(300).json({message: "Yet to be implemented"});
+
 });
+
+  function getBooks() {
+    return axios.get('http://example.com/api/books')
+        .then(response => {
+            console.log('Books displayed successfully:', response.data);
+            return response.data;
+        })
+        .catch(error => {
+            // Handle any errors thrown
+            console.error('Error fetching books:', error);
+             throw error;
+        });
+    }
+    
+    getBooks()
+        .then(books => {
+
+            console.log('List of books:', books);
+        })
+        .catch(error => {
+
+            console.error('Failed to get books:', error);
+        });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
@@ -43,6 +68,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
   return res.status(300).json({message: 'Book not found'});
   }
  });
+
+  function getIsbn() {
+      return axios.get('https://example/api/books')
+        .then(response => {
+            console.log('ISBN displayed successfully:', response.data);
+            return response.data;
+        })
+        .catch(error => {
+            console.error('Error showing ISBN:', error);
+            throw error;
+        });
+    }
+
+    getIsbn()
+       .then(isbn => {
+        
+         console.log('List of books by ISBN:', isbn);
+       })
+       .catch(error => {
+
+          console.error('Failed to get ISBN:', error);
+       });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -59,6 +106,28 @@ public_users.get('/author/:author',function (req, res) {
   }
 });
 
+  function getauthor() {
+    return axios.get('https://example.com/api/books')
+       .then(response => {
+           console.log('Author displayed successfully', response.data);
+           return response.data;
+       })
+       .catch (error => {
+         console.error('Error showing author:', error);
+          throw error;
+       });
+  }
+
+    getauthor()
+       .then(author => {
+
+        console.log('List of books by Author:', author);
+       })
+       .catch(error => {
+          
+          console.error('Failed to show Author:', error);
+       });
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   
@@ -73,6 +142,28 @@ public_users.get('/title/:title',function (req, res) {
       return res.status(300).json({message: "No book with that title found"});
     }
 });
+
+   function gettitle() {
+    return axios.get('https://example.com/api/books')
+       .then(response => {
+           console.log('Title displayed successfully', response.data);
+           return response.data;
+       })
+       .catch (error => {
+         console.error('Error showing Title:', error);
+          throw error;
+       });
+  }
+
+    gettitle()
+       .then(title => {
+
+        console.log('List of books by Title:', title);
+       })
+       .catch(error => {
+          
+          console.error('Failed to show Title:', error);
+       });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
